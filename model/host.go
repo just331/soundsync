3953c4431a3fa8a5cdc28f301a02e3d60cb97d4a
Joshua Johnson <joshuaj1397@gmail.com>
2019-02-10 19:27:51 -0600

    Added json fields for models

--- model/host.go
+++ model/host.go
@@ -5,7 +5,7 @@ import "github.com/go-bongo/bongo"
 // Host is the instance of a party and account for host of the party
 type Host struct {
 	bongo.DocumentBase `bson:",inline"`
-	SpotifyAuth        string   // Auth token for spotify
-	PhoneNum           string   // Phone number of host
-	Users              []string // Other users in this instance
+	SpotifyAuth        string   `json: "spotifyauth"` // Auth token for spotify
+	PhoneNum           string   `json: "phonenum"`    // Phone number of host
+	Users              []string `json: "users"`       // Other users in this instance
 }
