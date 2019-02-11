72036693e388c1b05cbb449ef599ce5e11ae575c
just331 <justinr331@gmail.com>
2019-02-11 16:08:02 -0600

    Added Roles model and added CRUD for Roles.

--- model/db.go
+++ model/db.go
@@ -137,3 +137,59 @@ func DeleteUserByPhoneNum(phoneNum string) error {
 	err = DeleteUser(user)
 	return err
 }
+
+// SaveRoles handles creating and updating user roles
+func SaveRoles(myRoles *Roles) error {
+	err := connection.Collection("roles").Save(myRoles)
+	if vErr, ok := err.(*bongo.ValidationError); ok {
+		log.Fatal(vErr.Errors)
+	} else {
+		log.Fatal(err.Error())
+	}
+	return err
+}
+
+// FindRoleByID handles IDing roles of user by their ID
+func FindRoleByID(id string) (*Roles, error) {
+	role := &Roles{}
+	err := connection.Collection("role").FindById(bson.ObjectIdHex(id), role)
+	return role, err
+}
+
+// FindRolesByPhoneNum returns a roles given the phone number of user
+func FindRolesByPhoneNum(phoneNum string) (*Roles, error) {
+	roles := &Roles{}
+	err := connection.Collection("roles").FindOne(bson.M{"PhoneNum:": phoneNum}, roles)
+	return roles, err
+}
+
+// DeleteRoles deletes a users roles
+func DeleteRoles(myRoles *Roles) error {
+	err := connection.Collection("role").DeleteDocument(myRoles)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err
+}
+
+// DeleteRolesByID deletes roles given a User id
+func DeleteRolesByID(id string) error {
+	roles, err := FindUserByID(id)
+	if err != nil {
+		return err
+	}
+
+	err = DeleteUser(roles)
+	return err
+}
+
+// DeleteUserByPhoneNum deletes roles given a user's phone number
+func DeleteRolesByPhoneNum(phoneNum string) error {
+	roles, err := FindUserByPhoneNum(phoneNum)
+	if err != nil {
+		return err
+	}
+
+	err = DeleteUser(roles)
+	return err
+}
