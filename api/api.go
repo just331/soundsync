58613fde242ef7ac97c6a11c1ec331dc10cc0648
Joshua Johnson <joshuaj1397@gmail.com>
2019-03-31 18:05:08 -0500

    Hand off to justin

--- api/api.go
+++ api/api.go
@@ -2,37 +2,12 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"time"
 
-	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/joshuaj1397/soundsync/model"
 )
-
-var mySigningKey = []byte("ASuperSecretSigningKeyCreatedByTheAliensFromArrival")
-
-var GetToken = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	/* Create the token */
-	token := jwt.New(jwt.SigningMethodHS256)
-
-	// Create a map to store our claims
-	claims := token.Claims.(jwt.MapClaims)
-
-	/* Set token claims */
-	claims["admin"] = true
-	claims["name"] = "Joshua Johnson"
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-
-	/* Sign the token with our secret */
-	tokenString, _ := token.SignedString(mySigningKey)
-	fmt.Println("I singed a token senpai!")
-
-	/* Finally, write the token to the browser window */
-	w.Write([]byte(tokenString))
-})
 
 // CreateParty returns the party code so the host can send it out to others
 var CreateParty = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
