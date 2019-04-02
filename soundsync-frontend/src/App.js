0d45e778b592d72d578a47aa9b6874bd8b6189b8
Joshua Johnson <joshuaj1397@gmail.com>
2019-04-02 18:21:26 -0500

    init for styling

--- soundsync-frontend/src/App.js
+++ soundsync-frontend/src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CreateParty from './Create';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const theme = createMuiTheme({
+  spacing: {
+    unit: 50,
+  },
+  text-align: center,
+});
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <h1>soundsync</h1>
+      <CreateParty/>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
--- soundsync-frontend/src/Create.js
+++ soundsync-frontend/src/Create.js
@@ -1,70 +1,66 @@
-import React, { Component } from "react";
-import Logo from './logo.png';
+import React, { Component, useState } from "react";
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = (theme) => {
+  return ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit,
+      width: 200,
+    },
+    dense: {
+      marginTop: 19,
+    },
+    menu: {
+      width: 200,
+    },
+  })
 
+};
 
-class Input extends Component {
-  constructor() {
-        super(props);
-        this.state = {
-            name: this.props.name,
-            open: false,
-            explicitswitch: true,
-            style : {width: 0}
-    };
-    }
+function CreateParty({ classes }) {
+  const [values, setValues] = useState({
+    nickName: '',
+    phoneNumber: '',
+    partyName: '',
+  });
 
-    render() {
-        return (
-          <input
-          name="Name"
-          type="tel"
-          placeholder="Enter Phone Number"
-          required="required"
-          value={this.state.numberOfGuests}
-          onChange={this.handleInputChange} />
-    );
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
   }
+
+  return (
+    <div>
+      <form>
+        <TextField
+          id="partyName"
+          label="Party Name"
+          className={classes.textField}
+          value={values.partyName}
+          onChange={handleChange('partyName')}
+        />
+        <TextField
+          id="phoneNumber"
+          label="Phone Number"
+          className={classes.textField}
+          value={values.phoneNumber}
+          onChange={handleChange('phoneNumber')}
+        />
+        <TextField
+          id="nickName"
+          label="Nick Name"
+          className={classes.textField}
+          value={values.nickName}
+          onChange={handleChange('nickName')}
+        />
+      </form>
+    </div>
+  )
 }
 
-class Create extends Component {
-  constructor() {
-        super();
-        this.state = {
-            customers: [],
-            open: false,
-            explicitswitch: true,
-            style : {width: 0}
-    };
-    }
-
-    render() {
-        return (
-        <div>
-        <img src={Logo} alt="soundsync Logo" class="logo"></img>
-            <h1>soundsync</h1>
-                <form method="post">
-                    <Input name="Josh">
-                    <input
-                    name="Name"
-                    type="string"
-                    placeholder="Enter Username"
-                    required="required"
-                    value={this.state.numberOfGuests}
-                    onChange={this.handleInputChange} />
-                    <input
-                    name="Name"
-                    type="string"
-                    placeholder="Enter Party Name"
-                    required="required"
-                    value={this.state.numberOfGuests}
-                    onChange={this.handleInputChange} />
-                    <br></br>
-                    <input type="submit" value="Send Code"></input>
-                </form>
-      </div>
-    );
-  }
-}
-
-export default Create;
+export default withStyles(styles)(CreateParty);
